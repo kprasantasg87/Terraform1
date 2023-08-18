c817267b56e2f69e3ca9cdd75306fdfaf6838261
kprasantasg87 <88620148+kprasantasg87@users.noreply.github.com>
2023-08-18 06:39:00 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,21 @@
+variable "region"{
+    default="ap-south-1"
+}
+variable "accesskey"{
+    default="AKIA2K5HQNFXC5AAX5ZA"
+}
+variable "secretkey"{
+    default="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"
+}
+variable "vpccidr"{
+   default="10.0.0.0/16"
+}
+variable "subdir"{
+    default="10.0.1.0/24"
+}
+variable "IGWCIDR"{
+    default="0.0.0.0/0"
+}
+variable "image"{
+    default="ami-072ec8f4ea4a6f2cf"
+}
